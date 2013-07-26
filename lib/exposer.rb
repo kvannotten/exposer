41ec9c2b5a69d67d7208b75d6bb1aef57f03b421
Kristof Vannotten <kristof@vannotten.be>
2013-07-26 11:59:06 +0800

    First fully functional version with key uploading support

--- lib/exposer.rb
+++ lib/exposer.rb
@@ -3,10 +3,12 @@ require "exposer/hash"
 require "net/http"
 require 'net/ssh'
 require "json"
-
+require 'fileutils'
 module Exposer
   
   def self.prepare
+    Exposer.make_sure_has_key
+    
     url = URI.parse("http://www.exposer.io/host")
     @response = JSON.parse(Net::HTTP.get(url))
     
@@ -29,6 +31,23 @@ module Exposer
     rescue
       raise "An error occurred while creating the tunnel, please try again later."
       exit!
+    end
+  end
+  
+  def self.make_sure_has_key
+    if not File.exists? File.expand_path("~/.exposer") then
+      if not File.exists? File.expand_path("~/.ssh/id_rsa.pub") then
+        raise "Could not find public key in ~/.ssh/id_rsa.pub"
+        exit!
+      else
+        key = IO.read(File.expand_path("~/.ssh/id_rsa.pub"))
+        response = JSON.parse(Net::HTTP.post_form(URI.parse("http://www.exposer.io/key"), { :key => key }).body)
+        if response.member? "error" then
+          raise "An error occurred while processing your public key: #{response.error}"
+          exit!
+        end
+        File.open(File.expand_path("~/.exposer"), "w") { |f| f.write "1" } 
+      end
     end
   end
   
